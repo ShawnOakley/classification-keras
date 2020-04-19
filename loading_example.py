2f9ff03b06b0d58d05e8dd403e8b7a681b61f763
Shawn Oakley <shawn.oakley@dominodatalab.com>
2020-04-18 22:11:06 -0400

    Adds loading example

--- loading_example.py
+++ loading_example.py
@@ -1,4 +1,3 @@
-# https://app.pluralsight.com/course-player?clipId=28da0493-613d-47c9-8ebb-d26d942e02bf
 import numpy as np
 import seaborn as sns
 from sklearn.model_selection import train_test_split
@@ -6,6 +5,14 @@ from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.utils import np_utils
+from keras.models import model_from_json
+
+json_file = open('iris_model.json', 'r')
+
+json_model = json_file.read()
+
+json_file.close()
+
 
 iris = sns.load_dataset('iris')
 iris.head()
@@ -26,17 +33,14 @@ def one_hot_encoder(array):
 one_train_data_y = one_hot_encoder(train_data_y)
 one_test_data_y = one_hot_encoder(test_data_y)
 
-model = Sequential()
+loaded_model = model_from_json(json_model)
 
-model.add(Dense(16, input_shape=(4,), name='Input_Layer', activation='relu'))
+loaded_model.load_weights)'iris_model.h5')
+print('loaded model from disk')
 
-model.add(Dense(3, name='Output_Layer', activation='softmax'))
+loaded_model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.summmary()
-
-model.compile(optimizer='adam', loss='categorical_crossentrpy', metric=['accuracy'])
-
-model.fit(train_data_X, one_train_data_y, epochs=10, batch_size=2, verbose=2)
-
-loss, accuracy = model.evaluate(test_data_X, one_test_data_y, verbose=0)
-
+def one_hot_encoder(array):
+    unique_values, indices = np.unique(array, return_inverse=True)
+    one_hot_encoded_data = np_utils.to_categorical(indices, len(unique_values))
+    return one_hot_encoded_data
